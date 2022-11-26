72413b655995cbfffd5d42b8d9d2ddf885eaabe3
Инна Кляхина <inna.galkina95@ya.ru>
2022-11-26 15:07:41 +0300

    v1.2

--- backend/registration/appeals/models.py
+++ backend/registration/appeals/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+    patronymic = models.CharField(
+        max_length=150,
+        verbose_name='Отчество'
+    )
+    phone = models.CharField(
+        max_length=11,
+        verbose_name='Номер телефона',
+        help_text='Введите номер телефона в формате 89001234567'
+    )
+    text = models.TextField(
+        verbose_name='Обращение',
+        help_text='Опишите вашу проблему'
+    )
+
+    def __str__(self):
+        return self.name
